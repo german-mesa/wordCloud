5097ace52714805de5979509059a6b088b1d2daa
german-mesa <german.mesa@sap.com>
2021-03-05 17:07:43 +0100

    Scrapping

--- src/Example_SuccessFactor_4_Data_Sampling.py
+++ src/Example_SuccessFactor_4_Data_Sampling.py
@@ -1,23 +1,22 @@
 # HANA Exercise
-import json
 import os
+import json
 
-import pandas as pd
-from hana_ml.dataframe import ConnectionContext, DataFrame, create_dataframe_from_pandas
+from hdbcli import dbapi
 
 query = """
     SELECT  
-        'Title',
-        'Requisition ID',
-        'Posted Date' as DATE,
-        'Recruiter',
-        'Region',
-        'Country',
-        'City',
-        'Work Area' as AREA,
-        'Expected Travel',
-        'Career Status',
-        'Employment Type'
+        "Title",
+        "Requisition ID",
+        "Posted Date" as POSTED,
+        "Recruiter",
+        "Region",
+        "Country",
+        "City",
+        "Work Area" as AREA,
+        "Expected Travel",
+        "Career Status",
+        "Employment Type"
     FROM    
         PUBLICDATA.POSITIONS
 """
@@ -33,15 +32,34 @@ def main():
     options = get_connection_options()
 
     # Construct a Explore Data client object.
-    with ConnectionContext(options['address'], options['port'], options['user'], options['password']) as connection:
-        # Execute query and count records
-        positions_df = connection.sql(query)
-        print('{0} records in this table'.format(positions_df.count()))
+    connection = dbapi.connect(
+        address=options['address'],
+        port=options['port'],
+        user=options['user'],
+        password=options['password']
+    )
 
-        # Execute query grouping by some criteria
-        query_by_region = "SELECT DATE, AREA, COUNT(*) FROM (" + query + ") GROUP BY DATE, AREA"
-        positions_by_region_df = connection.sql(query_by_region)
-        print('{0} records in this table'.format(positions_by_region_df.count()))
+    # Preparing cursor
+    cursor = connection.cursor()
+
+    # Execute query and count records
+    cursor.execute(query)
+    positions_df = cursor.fetchall()
+    print('{0} records in this table'.format(len(positions_df)))
+
+    # Execute query grouping by some criteria
+    query_by_region = "SELECT POSTED, AREA, COUNT(*) " + \
+                      "FROM (" + query + ") " + \
+                      "GROUP BY POSTED, AREA " + \
+                      "ORDER BY POSTED DESC, AREA ASC"
+
+    cursor.execute(query_by_region)
+    positions_by_region_df = cursor.fetchall()
+    print('{0} records in this table'.format(len(positions_by_region_df)))
+
+    # Close the cursor
+    cursor.close()
+    connection.close()
 
 
 if __name__ == '__main__':
